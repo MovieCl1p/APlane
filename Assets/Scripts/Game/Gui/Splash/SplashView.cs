aad483915c6e591c358fef0420270ec603e221dd
Anton Ametov <antonametov88@gmail.com>
2018-01-07 15:39:16 +0200

    Fix splash

--- Assets/Scripts/Game/Gui/Splash/SplashView.cs
+++ Assets/Scripts/Game/Gui/Splash/SplashView.cs
@@ -9,17 +9,20 @@ namespace Game.Gui.Splash
     public class SplashView : BaseView
     {
         [SerializeField]
-        private Transform _logo;
+        private Image _logo;
+
+        [SerializeField]
+        private Image _bg;
 
         protected override void Start()
         {
             base.Start();
-            ScheduleUpdate(3, false);
+            ScheduleUpdate(2, false);
 
-            var tween = _logo.DOScale(1, 1);
+            _logo.DOFade(1, 1);
+            _bg.DOFade(1, 1);
             
-            
-            _logo.DOShakePosition(2, 2, 20, 90, true);
+            _logo.transform.DOScale(1, 1);
         }
 
         protected override void OnScheduledUpdate()
